872f8ea440d1a56af57a4a4c8d5fcbdff4bfac84
Cesar Mayo <cesarmayo@Cesars-MacBook-Pro.local>
2023-09-03 20:41:48 -0300

    fix(google-login): add env to google api redirect

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,12 @@
 import Link from "next/link";
 
 const Home = () => {
-const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL;
   return <div>
     <div className="flex items-center justify-center h-screen">
       <div className="bg-white shadow-sm shadow-inner rounded-2xl p-16">
         <ul style={{ listStyleType: 'none' }}>
           <Link
-              href="http://localhost:1337/api/connect/google"
+              href={`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/connect/google`}
               passHref={true}
               className="border-2 border-gray-600 mt-8 text-gray-600 font-semibold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
           >
