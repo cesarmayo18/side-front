65167d9381220089136441bfacec75d5dadf7857
Cesar Mayo <cesarmayo@Cesars-MacBook-Pro.local>
2023-09-03 22:42:46 -0300

    fix(login) fix login callback

--- src/app/callback/page.js
+++ src/app/callback/page.js
@@ -3,10 +3,12 @@ import {useEffect, useState} from "react";
 
 const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL;
 
-const LoginRedirect = ({searchParams}) => {
+const LoginRedirect = (props) => {
     const [user, setUser] = useState(null);
+    const [query, setQuery] = useState(props.searchParams);
+    const searchParams = props.searchParams
+    console.log(props, "<<<<<<<")
     useEffect(() => {
-        console.log(`${backendUrl}/api/auth/google/callback?id_token=${searchParams.id_token}&access_token=${searchParams.access_token}`)
         fetch(`${backendUrl}/api/auth/google/callback?id_token=${searchParams.id_token}&access_token=${searchParams.access_token}`)
             .then(res => {
                 if (res.status !== 200) {
@@ -16,19 +18,15 @@ const LoginRedirect = ({searchParams}) => {
             })
             .then(res => res.json())
             .then(res => {
-                console.log(res)
                 setUser(res.user)
-                // Successfully logged with Strapi
-                // Now saving the jwt to use it for future authenticated requests to Strapi
                 localStorage.setItem('jwt', res.jwt);
                 localStorage.setItem('username', res.user.username);
-                // setTimeout(() => history.push('/'), 3000); // Redirect to homepage after 3 sec
             })
             .catch(err => {
                 console.log(err);
                 setText('An error occurred, please see the developer console.')
             });
-    }, [searchParams]);
+    }, [query]);
 
     return <p>{JSON.stringify(user)}</p>
 };
